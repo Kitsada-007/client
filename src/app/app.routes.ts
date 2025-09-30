62b726163db5db276e9be101611b9bccb9660b93
Kitsada-007 <uroroom123@gmail.com>
2025-09-30 22:03:52 +0700

    router-page-up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { Register } from './pages/auth/register/register';
 import { Profile } from './pages/user/profile/profile';
 import { Collection } from './pages/user/collection/collection';
 import { DetailGame } from './pages/user/detail-game/detail-game';
-import { Money } from './pages/user/money/money';
+
 import { ShoppingCart } from './pages/user/shopping-cart/shopping-cart';
 import { AddMonny } from './pages/user/history/add-monny/add-monny';
 import { BuyGram } from './pages/user/history/buy-gram/buy-gram';
@@ -14,6 +14,7 @@ import { Add } from './pages/admin/home-admin/add/add';
 import { Edit } from './pages/admin/home-admin/edit/edit';
 import { Delete } from './pages/admin/home-admin/delete/delete';
 import { ProfileAdmin } from './pages/admin/profile-admin/profile-admin';
+import { Wallet } from './pages/user/wallet/wallet';
 
 export const routes: Routes = [
   // User-path
@@ -23,7 +24,7 @@ export const routes: Routes = [
   { path: 'profile', component: Profile },
   { path: 'collection', component: Collection },
   { path: 'detail-game', component: DetailGame },
-  { path: 'money', component: Money },
+  { path: 'wallet', component: Wallet },
   { path: 'shopping', component: ShoppingCart },
   {
     path: 'history',
