bca547ec8962edca4f973de1291d9b17c0545171
PalmKitsanaphon <Kitsanaphon.sukdee@gmail.com>
2025-10-01 23:49:24 +0700

    update PageLogin

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
