bbafcce48beeb331172a66ef098880836153ccf5
PalmKitsanaphon <Kitsanaphon.sukdee@gmail.com>
2025-10-02 00:00:54 +0700

    update PageRegister

--- src/app/pages/auth/register/register.ts
+++ src/app/pages/auth/register/register.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatHint } from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [MatToolbarModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, MatHint],
   templateUrl: './register.html',
   styleUrl: './register.scss'
 })
