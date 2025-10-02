cbb6663affb91f14c6ffdac88e12db75344aeba0
PalmKitsanaphon <Kitsanaphon.sukdee@gmail.com>
2025-10-02 23:56:32 +0700

    update UI

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
